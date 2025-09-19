87bd9eb58a56d31fd20d1aac0684be0f14a10107
ShemarBoyd <116024194+marii9@users.noreply.github.com>
2025-09-19 15:59:41 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="flex h-screen items-center justify-center bg-yellow-900">
       <ContactForm />
       <h1 className="text-5xl font-bold text-white">
-        🚀 Tailwind is working!
+        Aws Project
       </h1>
     </div>
   );
